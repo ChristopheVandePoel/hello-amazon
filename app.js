ad0e37b953a5ccc1424e331759fbf8f3dc4c152b
christophe <christophe@ksdc.be>
2016-10-31 22:49:55 +0000

    opening commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var proxy = require('express-http-proxy');
+
+app.use('/proxy', proxy('www.google.com', {
+//  forwardPath: function(req, res) {
+//    return require('url').parse(req.url).path;
+//  }
+}));
+
+
+app.get("/",function(req,res){
+        res.send("<h1>Hey lisa!</h1>");
+});
+
+app.listen(80);
